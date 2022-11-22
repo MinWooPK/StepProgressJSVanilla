133eb42fe50b48eef9de57b356d2bba1f603e244
MinWooPK <minwoowork91@gmail.com>
2022-11-22 10:01:27 +0100

    2ndFinal

--- assets/main.js
+++ assets/main.js
@@ -4,46 +4,37 @@ const next = document.getElementById("next");
 const progress = document.getElementById("progress");
 const activos = document.querySelectorAll(".active");
 let Numero = 1 /*No Puede ser 0 , debido a que solo hay 3 steps 1+1=2 , 2+1=3 y 3+1=4 */
+
+// Progreción o decreción
+
 next.addEventListener('click', () => {
     Numero++;
     if (Numero > circulo.length) {
         Numero = circulo.length
-
     }
     actualizado();
 });
-
-
-
 
 previos.addEventListener('click', () => {
     Numero--;
     if (Numero < 1) {
         Numero = 1;
     }
-
     actualizado();
 });
 
-const actualizado = () => {
+// ACC
 
+const actualizado = () => {
     circulo.forEach((circle, i) => {
         if (i < Numero) {
-
             circle.classList.add("active");
-
         } else {
-
             circle.classList.remove("active")
         }
-
-
     });
 
-
-
-
-
+//  Progress Step Bar
 
 progress.style.width = 
     ((Numero - 1) / (circulo.length - 1)) * 100 + "%";
